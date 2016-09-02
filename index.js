d1017e755655eba9648021f12a747f7d8ec5b6e6
Camille Douay <camille.douay@gmail.com>
2016-09-02 15:47:27 +0200

    syntax error correction

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ app.post('/webhook', function (req, res) {
     for (i = 0; i < events.length; i++) {
         var event = events[i];
         if (event.message && event.message.text) {
-            sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+            FB.sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
         }
     }
     res.sendStatus(200);
